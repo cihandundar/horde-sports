426b9a5b4b4eca57540c220d307c5de7d6a9ee72
Cihan Dündar <dundarcihan97@hotmail.com>
2025-12-19 00:54:00 +0300

    banner section added

--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -4,6 +4,7 @@
 <div class="home-page">
     @include('front.partials.matches-slider')
     
+    @include('front.partials.ad-banner')
 </div>
 @endsection
 
---
+++ resources/views/front/partials/ad-banner.blade.php
@@ -0,0 +1,16 @@
+<section class="ad-banner-section">
+    <div class="ad-banner-container">
+        <div class="ad-banner-wrapper">
+            <a href="#" class="ad-banner-link" target="_blank" rel="noopener noreferrer">
+                <div class="ad-banner-content">
+                    <div class="ad-banner-label">
+                        <i class="fas fa-ad"></i>
+                        <span>Reklam</span>
+                    </div>
+                    <img src="{{ asset('front/assets/images/banner.png') }}" alt="Reklam Banner" class="ad-banner-image">
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
+
